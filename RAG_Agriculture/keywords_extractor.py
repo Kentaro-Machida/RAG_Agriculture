659e44a689fd7b4f23388fee6b79b462a95fb709
Kentaro Machida <kenkenm120319@gmail.com>
2024-08-29 12:45:30 +0700

    Added dummy key words extractor

--- RAG_Agriculture/keywords_extractor.py
+++ RAG_Agriculture/keywords_extractor.py
@@ -1,3 +1,37 @@
 """
 ユーザーから入力された質問文からLLMでキーワードを抜き出すためのモジュール
 """
+class DummyKeywordExtractor:
+    def __init__(self):
+        pass
+
+    def extract_keywords(self, question: str) -> dict:
+        return {
+            "PURPOSE": "発芽安定化",
+            "ACTION": "",
+            "TARGET": "種子",
+            "SUBTARGET": "",
+            "LOCATION": "水田",
+            "METHOD": "",
+            "MATERIAL": "",
+            "CROP_EXAMPLE": "イネ",
+            "REGION": "",
+            "CONDITION": "",
+            "TASK_NAME": "",
+            "URI": ""
+        }
+    
+
+def test():
+    # テキストデータの読み込み
+    text_path = './tests/keywords_extractor/input.txt'
+    with open(text_path, 'r') as f:
+        question = f.read()
+        
+    keyword_extractor = DummyKeywordExtractor()
+    keywords = keyword_extractor.extract_keywords(question)
+    print(keywords)
+
+
+if __name__ == '__main__':
+    test()
