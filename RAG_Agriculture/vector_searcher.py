7ea607f01762d85a5664c642034dc97074d1b263
Kentaro Machida <kenkenm120319@gmail.com>
2024-08-29 12:33:25 +0700

    Added waviate sample and made vector searcher dummy

---
+++ RAG_Agriculture/vector_searcher.py
@@ -0,0 +1,45 @@
+"""
+キーワード群からベクトル検索により関連情報を取得
+"""
+import polars as pl
+import json
+
+class DummyVectorSearcher:
+    def __init__(self):
+        pass
+
+    def search(self, keywords:dict, n=10)->list:
+        '''
+        Input: Key words dict with black
+        Output List of Key words dicts filled with related information
+        '''
+        return [
+            {
+                "PURPOSE": "発芽安定化",
+                "ACTION": "まぶす",
+                "TARGET": "種子",
+                "SUBTARGET": "",
+                "LOCATION": "水田",
+                "METHOD": "",
+                "MATERIAL": "鉄",
+                "CROP_EXAMPLE": "イネ",
+                "REGION": "",
+                "CONDITION": "",
+                "TASK_NAME": "鉄コーティング",
+                "URI": "http://cavoc.org/aao/ns/4/A51"
+            }
+        ] * n
+
+
+def test():
+    test_path = './tests/vector_searcher/input.json'
+    with open(test_path, 'r') as f:
+        keywords = json.load(f)
+    
+    vector_searcher = DummyVectorSearcher()
+    result = vector_searcher.search(keywords)
+    print(result)
+
+
+if __name__ == '__main__':
+    test()
