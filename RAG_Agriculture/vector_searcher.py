5a2bbb17f31c1c658d7081f3cff0dbcdc264263e
Kentaro Machida <kenkenm120319@gmail.com>
2024-09-04 23:04:19 +0700

    Implemented basic vector searcher

--- RAG_Agriculture/vector_searcher.py
+++ RAG_Agriculture/vector_searcher.py
@@ -4,6 +4,13 @@ JSONでキーワードを受け取り、検索結果をJSONもしくはJSONの�
 """
 import polars as pl
 import json
+import os
+from utils.data_load import load_json, json2str
+from openai import OpenAI
+import weaviate
+import weaviate.classes.config as wc
+import weaviate.classes.query as wq
+
 
 class DummyVectorSearcher:
     def __init__(self):
@@ -11,7 +18,7 @@ class DummyVectorSearcher:
 
     def search(self, keywords:dict, n=10)->list:
         '''
-        Input: Key words dict with black
+        Input: Key words dict with blanck
         Output List of Key words dicts filled with related information
         '''
         return [
@@ -32,14 +39,43 @@ class DummyVectorSearcher:
         ] * n
 
 
+class VectorSearcher:
+
+    def __init__(self, embedding_model:str):
+        self.embedding_model = embedding_model
+
+    def search(self, keywords:dict, n=2)->list:
+        keywords_list = []
+        str_keywords = json.dumps(keywords)
+
+        if self.embedding_model == 'openai':
+            api_key = os.environ.get('OPENAI_API_KEY')
+            headers = {"X-OpenAI-Api-Key": api_key}  # Replace with your OpenAI API key
+
+            try:
+                client = weaviate.connect_to_local(headers=headers)
+
+                agris = client.collections.get("Agriculture")
+                
+                response = agris.query.near_text(
+                    query=str_keywords, limit=n, return_metadata=wq.MetadataQuery(distance=True)
+                )
+
+            finally:
+                client.close()
+            
+            keywords_list = [o.properties for o in response.objects]
+            return keywords_list
+
+
 def test():
     test_path = './tests/vector_searcher/input.json'
     with open(test_path, 'r') as f:
         keywords = json.load(f)
     
-    vector_searcher = DummyVectorSearcher()
-    result = vector_searcher.search(keywords)
-    print(result)
+    vector_searcher = VectorSearcher(embedding_model='openai')
+    keywords_list = vector_searcher.search(keywords)
+    print(keywords_list)
 
 
 if __name__ == '__main__':
