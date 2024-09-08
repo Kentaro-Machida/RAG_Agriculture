24a8e2a28c7216ba05648e4773f78f2ffe75323f
Kentaro Machida <kenkenm120319@gmail.com>
2024-09-08 14:36:51 +0700

    Transformers を使用したベクトル検索を実装

--- RAG_Agriculture/vector_searcher.py
+++ RAG_Agriculture/vector_searcher.py
@@ -2,13 +2,14 @@
 キーワード群からベクトル検索により関連情報を取得
 JSONでキーワードを受け取り、検索結果をJSONもしくはJSONのリストで返す
 """
+from transformers import AutoTokenizer, AutoModel
 import polars as pl
 import json
 import os
-from .utils.data_load import load_json, json2str
-from openai import OpenAI
+import sys
+from .utils.embedding_process import text_embedding
+from .utils.text_preprocess import mE5_preprocess, keywords2text
 import weaviate
-import weaviate.classes.config as wc
 import weaviate.classes.query as wq
 
 
@@ -50,6 +51,7 @@ class VectorSearcher:
         str_keywords = json.dumps(keywords)
 
         if self.embedding_model == 'openai':
+            print("Embedding model: openai")
             api_key = os.environ.get('OPENAI_API_KEY')
             headers = {"X-OpenAI-Api-Key": api_key}  # Replace with your OpenAI API key
 
@@ -64,9 +66,35 @@ class VectorSearcher:
 
             finally:
                 client.close()
+        
+        elif self.embedding_model == 'intfloat/multilingual-e5-large':
+            print("Embedding model: intfloat/multilingual-e5-large")
+            model = AutoModel.from_pretrained(self.embedding_model)
+            model.eval()
+            tokenizer = AutoTokenizer.from_pretrained(self.embedding_model)
             
-            keywords_list = [o.properties for o in response.objects]
-            return keywords_list
+            try:
+                client = weaviate.connect_to_local()
+                print(f'------connected to {self.connection_name}------')
+                agris = client.collections.get(self.connection_name)
+
+                json_str = keywords2text(keywords)
+                me5_input = mE5_preprocess(json_str, 'query')
+                embed_vector = text_embedding([me5_input], model, tokenizer).detach().numpy()[0]
+                
+                response = agris.query.near_vector(
+                    near_vector=embed_vector, limit=n
+                )
+
+            finally:
+                client.close()
+        
+        else:
+            print('Invalid embedding model. Please check embedding_model in the config file')
+            sys.exit()
+            
+        keywords_list = [o.properties for o in response.objects]
+        return keywords_list
 
 
 def test():
@@ -74,7 +102,7 @@ def test():
     with open(test_path, 'r') as f:
         keywords = json.load(f)
     
-    vector_searcher = VectorSearcher(embedding_model='openai')
+    vector_searcher = VectorSearcher(connection_name='Agriculture' ,embedding_model='intfloat/multilingual-e5-large')
     keywords_list = vector_searcher.search(keywords)
     print(keywords_list)
 
