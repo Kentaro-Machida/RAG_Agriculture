8f25d0afef8cb5c09d53013f18a0beaf9336f120
Kentaro Machida <kenkenm120319@gmail.com>
2024-09-09 17:39:04 +0700

    翻訳機能の追加

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ from RAG_Agriculture import answer_generator as ag, keywords_extractor as ke, ve
 from RAG_Agriculture.utils.data_load import load_json, add_lang_to_promptpath
 from langdetect import detect
 
-# # load config.json
-# config = load_json('./config.json')
-# retrieve_llm = config['retrieve_llm']
-# extract_keywords_prompt_path = config['extract_keywords_prompt_path']
-# generate_llm = config['generate_llm']
-# generate_answer_prompt_path = config['generate_answer_prompt_path']
-# embedding_model = config['embedding_model']
-# search_num = config['search_num']
 
 def main():
+
+    # load config.json
+    config = load_json('./config.json')
+    retrieve_llm = config['retrieve_llm']
+    extract_keywords_prompt_path = config['extract_keywords_prompt_path']
+    generate_llm = config['generate_llm']
+    generate_answer_prompt_path = config['generate_answer_prompt_path']
+    embedding_model = config['embedding_model']
+    search_num = config['search_num']
 
     # Input text by conversation system
     question = input("Please input your question: ")
@@ -68,7 +69,7 @@ def test():
     # Translate keywords if the language is not Japanese
     if lang != 'ja':
         print(f"Translate from {lang} to ja.")
-        url = 'http://localhost:5001/translate'
+        url = 'http://localhost:5001/translate/to_ja'
         headers = {'Content-Type': 'application/json'}
         response = requests.post(url, headers=headers, json={"keywords": keywords, "lang": lang})
         if response.status_code == 200:
@@ -83,7 +84,24 @@ def test():
     print("Embedding model:", embedding_model)
     print("Connection name:", config['weaviate']['schema']['class'])
     keywords_list = vector_searcher.search(keywords, n=search_num)
-    print("Vector search reslts:", keywords_list)
+    print("Vector search results:", keywords_list)
+
+    # Translate keywords back to the original language
+    if lang != 'ja':
+        print("-------------------")
+        print(f"Translate from ja to {lang}.")
+        url = 'http://localhost:5001/translate/from_ja'
+        headers = {'Content-Type': 'application/json'}
+        for i, keywords in enumerate(keywords_list):
+            response = requests.post(url, headers=headers, json={"keywords": keywords, "lang": lang})
+            if response.status_code == 200:
+                trans_keywords = response.json()  # JSONレスポンスを辞書型として取得
+                keywords_list[i] = trans_keywords
+            else:
+                print(f"Error: {response.status_code}")
+        
+        print("Translated keywords:", keywords_list)
+
 
     # Generate answer
     answer_generator = ag.AnswerGenerator(generate_answer_prompt_path, generate_llm)
